64c5787f32b6c11927999141889ef92eb12fb410
Antonio Martinović <martinovic.antonio@gmail.com>
2019-05-05 23:16:17 +0200

    Add basic chaincode implementation

---
+++ chaincode/chaincode.go
@@ -0,0 +1,86 @@
+package chaincode
+
+import (
+	"github.com/TopHatCroat/hlf-contractor/chaincode/schema"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/pkg/errors"
+	"github.com/s7techlab/cckit/extensions/debug"
+	"github.com/s7techlab/cckit/extensions/owner"
+	"github.com/s7techlab/cckit/identity"
+	"github.com/s7techlab/cckit/router"
+	"github.com/s7techlab/cckit/router/param/defparam"
+	"github.com/s7techlab/cckit/state/mapping"
+)
+
+var (
+	StateMappings = mapping.StateMappings{}.Add(
+		&schema.Project{},
+		mapping.PKeySchema(&schema.ProjectId{}),
+		mapping.List(&schema.ProjectList{}),
+	)
+	EventMappings = mapping.EventMappings{}
+)
+
+func NewCC() *router.Chaincode {
+	r := router.New(`project`)
+
+	// Mappings for chaincode state
+	r.Use(mapping.MapStates(StateMappings))
+	// Mappings for chaincode events
+	r.Use(mapping.MapEvents(EventMappings))
+
+	// Store info about the chaincode initiator
+	r.Init(owner.InvokeSetFromCreator)
+
+	// method for debug chaincode state
+	debug.AddHandlers(r, `debug`, owner.Only)
+
+	r.
+		// Read all method
+		Query(`list`, queryProjectList).
+		// Get Project method, takes 2 params: Issuer Id and Project Name
+		Query(`get`, queryProject, defparam.Proto(&schema.ProjectId{})).
+
+		// txn methods
+		Invoke(`issue`, invokeProjectPublish, defparam.Proto(&schema.PublishProject{}))
+
+	return router.NewChaincode(r)
+}
+
+func queryProjectList(c router.Context) (interface{}, error) {
+	return c.State().List(&schema.Project{})
+}
+
+func queryProject(c router.Context) (interface{}, error) {
+	id := c.Param().(*schema.Project)
+	return c.State().Get(id)
+}
+
+func invokeProjectPublish(c router.Context) (res interface{}, err error) {
+	publishData := c.Param().(*schema.PublishProject)
+
+	// Validate the input message defined in schema
+	if err = publishData.Validate(); err != nil {
+		return nil, errors.Wrap(err, "Payload invalid")
+	}
+
+	issuer, err := identity.FromStub(c.Stub())
+
+	// Create state entry
+	project := &schema.Project{
+		Issuer:         issuer.GetID(),
+		Name:           publishData.Name,
+		OpenDate:       ptypes.TimestampNow(),
+		StartDate:      publishData.StartDate,
+		EndDate:        publishData.EndDate,
+		EstimatedValue: publishData.EstimatedValue,
+		State:          schema.Project_OPEN, // Initial state
+		Description:    publishData.Description,
+	}
+
+	if err = c.Event().Set(project); err != nil {
+		return nil, err
+	}
+
+	return project, c.State().Insert(project)
+}
--- chaincode/go.mod
+++ chaincode/go.mod
@@ -3,7 +3,8 @@ module github.com/TopHatCroat/hlf-contractor/chaincode
 go 1.12
 
 require (
-	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/golang/protobuf v1.3.1
 	github.com/mwitkow/go-proto-validators v0.0.0-20190212092829-1f388280e944
+	github.com/pkg/errors v0.8.1
+	github.com/s7techlab/cckit v0.4.5
 )
