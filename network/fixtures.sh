bc8eaee0f63514915792027d70a042572fa744af
Antonio Martinović <martinovic.antonio@gmail.com>
2019-08-08 22:27:56 +0200

    Save created users certificates

--- network/fixtures.sh
+++ network/fixtures.sh
@@ -7,18 +7,30 @@ set -e
 
 FABRIC_CA_CLIENT_HOME=./ca-client-home
 mkdir -p $FABRIC_CA_CLIENT_HOME
+rm -rf $FABRIC_CA_CLIENT_HOME/msp
 
-fabric-ca-client enroll --home $FABRIC_CA_CLIENT_HOME --url 'https://admin:adminpw@ca.awesome.agency:7054' 
+fabric-ca-client enroll --home $FABRIC_CA_CLIENT_HOME --url https://admin:adminpw@ca.awesome.agency:7054
 
 echo "Admin enrolled"
 
 for user in "user1" "user2"; do
+    username=$user@mail.com
 
-    fabric-ca-client register --id.name $user@mail.com --id.secret=password \
+    fabric-ca-client register --id.name $username --id.secret=password \
                     --home $FABRIC_CA_CLIENT_HOME \
                     --id.attrs '"hf.Registrar.Roles=client","rights=consume:ecert"' \
-                    --url https://ca.awesome.agency:7054
+                    --url https://ca.awesome.agency:7054 \
+
+    echo "User $user registered"
+
+    fabric-ca-client enroll --home $FABRIC_CA_CLIENT_HOME \
+                            --url https://$username:password@ca.awesome.agency:7054 \
+                            --mspdir $username
 
     echo "User $user enrolled"
-
 done
+
+echo
+echo "Users created:"
+fabric-ca-client identity list --home $FABRIC_CA_CLIENT_HOME \
+                               --url https://admin:adminpw@ca.awesome.agency:7054
