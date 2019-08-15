d3f1101e08f24b3774787f3d332f4cc7e39151c0
Antonio Martinović <martinovic.antonio@gmail.com>
2019-08-15 17:38:33 +0200

    More inergration tests

--- api/testdata/_common.sh
+++ api/testdata/_common.sh
@@ -2,12 +2,19 @@
 
 print_usage() {
 	echo "  Usage: "
-	echo "  API_HOST GET|POST|PUT|DELETE endpoint [json_data]"
+	echo "  GET|POST|PUT|DELETE endpoint [json_data]"
+	echo
+	echo "  Environmental variables:"
+	echo "    API_HOST - base host URL"
+	echo "    API_AUTHORIZATION - Authorization token"
+	echo "    VERBOSE -  Verbose option for curl"
+	echo "    PRETTY -  Output format, options: jq, python"
 }
 
 API_HOST=${API_HOST:-''}
+API_AUTHORIZATION=${API_AUTHORIZATION:-''}
 VERBOSE=${VERBOSE:-'s'}
-PRETTY=${PRETTY:-'python'}
+PRETTY=${PRETTY:-'jq'}
 
 if [[ -z "$API_HOST" ]]; then echo "You must set env var API_HOST"; exit 1; fi
 
@@ -15,18 +22,22 @@ GET(){
     if [[ -z "$1" ]]; then print_usage; exit 1; fi
 
     SHOW "`
-          curl -${VERBOSE} -X GET -H "Content-Type: application/json"  \
-          ${API_HOST}$1
-         `"
+    	curl -${VERBOSE} -X GET \
+        -H "Content-Type: application/json" \
+        -H "Authorization: API_AUTHORIZATION" \
+        ${API_HOST}$1
+        `"
 }
 
 DELETE(){
     if [[ -z "$1" ]]; then print_usage; exit 1; fi
 
     SHOW "`
-         curl -${VERBOSE} -X DELETE -H "Content-Type: application/json" \
-         ${API_HOST}$1
-        `"
+    	curl -${VERBOSE} -X DELETE \
+        -H "Content-Type: application/json" \
+	   	-H "Authorization: API_AUTHORIZATION" \
+        ${API_HOST}$1
+		`"
 }
 
 POST(){
@@ -35,8 +46,9 @@ POST(){
     if [[ -z "$2" ]]; then print_usage; exit 1; fi
 
     SHOW "`
-        curl -${VERBOSE} -X POST -H "Content-Type: application/json"  -d "$2" \
-             ${API_HOST}$1
+    	curl -${VERBOSE} -X POST -H "Content-Type: application/json"  -d "$2" \
+	   	-H "Authorization: API_AUTHORIZATION" \
+        ${API_HOST}$1
         `"
 }
 
@@ -46,19 +58,21 @@ PUT(){
     if [[ -z "$2" ]]; then print_usage; exit 1; fi
 
     SHOW "`
-          curl -${VERBOSE} -X PUT -H "Content-Type: application/json"  -d "$2"\
-          ${API_HOST}$1
-         `"
+    	curl -${VERBOSE} -X PUT \
+    	-H "Content-Type: application/json"  -d "$2"\
+ 	   	-H "Authorization: API_AUTHORIZATION" \
+        ${API_HOST}$1
+        `"
 }
 
 SHOW() {
-  PRETTY=${PRETTY:-''}
+  	PRETTY=${PRETTY:-''}
 
-  if [[ -z "$PRETTY" ]]; then
-    echo "$1"
-  elif [[ "$PRETTY" == "python" ]]; then
-    printf '%s' "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
-  else
-    echo $1 | jq
-  fi
+ 	if [[ -z "$PRETTY" ]]; then
+		echo "$1"
+  	elif [[ "$PRETTY" == "python" ]]; then
+    	printf '%s' "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
+  	else
+    	echo $1 | jq
+  	fi
 }
--- api/testdata/integration/run_all.sh
+++ api/testdata/integration/run_all.sh
@@ -2,6 +2,7 @@
 
 echo "Running full API integration tests"
 
-
 ./register_user.sh
 ./login_user.sh
+./get_me.sh
+./get_all_users.sh.sh
